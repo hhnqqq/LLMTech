1a1562e4eb4673a063c038746f7352a2e220ca44
hhnqqq <59022162+hhnqqq@users.noreply.github.com>
2023-08-02 16:34:44 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,9 @@
 from RoPE import *
 from beam_search import *
+
+def top_p(projected_vec: torch.Tensor, top_p = 0.1):
+    current_n_prob = projected_vec.squeeze(0)
+    _, current_n_indexes = current_n_prob[-1].sort(descending=True)
+    current_n_indexes = current_n_indexes[:int(current_n_indexes.shape[-1] * top_p)]
+    next_sambol = random.choice(current_n_indexes.tolist())
+    return next_sambol
